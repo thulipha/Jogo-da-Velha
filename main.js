b9bcb8921ddec1f78a3ab0a9d8fc9225a5f825b6
thulikito501@gmail.com <thulikito501@gmail.com>
2023-09-13 06:30:09 -0300

    criação de um "computador" pro jogo. Em desenvolvimento!

--- main.js
+++ main.js
@@ -30,107 +30,138 @@ const combinacoes = [
   
 
 
-
-function jogar(){
-  
-  if(game_over === false){
-    
-    if(this.classList.contains('X') || this.classList.contains('O')){
-      return false;
-     }
-     
-    else{
-      
-      this.classList.add(turno);
-      this.innerText = turno;
-      
-      checarVencedor();
-      mudarTurno();
-      jogadorAtual.innerText = turno;
-     };
-  };
-  if(game_over === true || empate === true){
-    return reiniciarJogo();
-  };
-  
-  function mudarTurno(){
-    turno === jogador ? turno = jogador2 : turno = jogador;
-  };
-  
-  function checarVencedor() {
-      const vencedor = combinacoes.some((comb) => {
-    
-        return comb.every((posicao) => {
-    
-          return tabuleiro[posicao].classList.contains(turno);
-        });
-      });
-      if (vencedor) {
-        vencedorTexto.innerText = turno;
-        return game_over = true;
-    
-      }
-      else {
-        return checarEmpate();
-      };
-    };
-    
-  function checarEmpate() {
-      let x = 0;
-      let o = 0;
-    
-      tabuleiro.forEach((item) => {
-        if (item.classList.contains('X'))
-          x++;
-    
-        if (item.classList.contains('O'))
-          o++;
-    
-        return x + o == 9 ? empatar() : false;
-      });
-    };
-    
-  function empatar() {
-      empate = true;
-      vencedorTexto.innerText = "Ninguém, Jogo empatado";
-    
-    };
-
-  function reiniciarJogo(){
-      
-      gameOverPage.style.display = 'block';
-      
-      
-      finalizar_jogo.forEach((item)=>{
-        
-        item.addEventListener('click', handleChoice);
-      });
-    };
-    
-  function handleChoice(e){
-        if(e.target.id === 'yes') {
-          
-          document.querySelector('.timer-count').style.display = 'block';
-          
-          this.removeEventListener('click', handleChoice);
-          return recarregarPagina();
-        };
-    };
-    
-  function recarregarPagina(){
-      clock--;
-      document.querySelector('#timer').innerText = clock;
-      
-      if(clock === 0)
-        return location.reload();
-        
-      return timer = setTimeout(recarregarPagina,1000);
-    };
-};
-
-
-
 tabuleiro.forEach((itemClicado) =>{
   
   itemClicado.addEventListener('click', jogar);
+  
+  function jogar(){
+    
+    if(game_over === false){
+      
+      if(this.classList.contains('X') || this.classList.contains('O')){
+        return false;
+       }
+       
+      else{
+        
+        this.classList.add(turno);
+        this.innerText = turno;
+        
+        checarVencedor();
+        mudarTurno();
+        jogadorAtual.innerText = turno;
+       };
+    };
+    if(game_over === true || empate === true){
+      return reiniciarJogo();
+    };
+    
+    function mudarTurno(){
+      turno === jogador ? bot() : turno = jogador;
+    };
+    
+    function checarVencedor() {
+        const vencedor = combinacoes.some((comb) => {
+      
+          return comb.every((posicao) => {
+      
+            return tabuleiro[posicao].classList.contains(turno);
+          });
+        });
+        if (vencedor) {
+          vencedorTexto.innerText = turno;
+          return game_over = true;
+      
+        }
+        else {
+          return checarEmpate();
+      };
+      };
+      
+    function checarEmpate() {
+        let x = 0;
+        let o = 0;
+      
+        tabuleiro.forEach((item) => {
+          if (item.classList.contains('X'))
+            x++;
+      
+          if (item.classList.contains('O'))
+            o++;
+      
+          return x + o == 9 ? empatar() : false;
+        });
+      };
+      
+    function empatar() {
+        empate = true;
+        vencedorTexto.innerText = "Ninguém, Jogo empatado";
+      };
+  
+    function reiniciarJogo(){
+        
+        gameOverPage.style.display = 'block';
+        
+        
+        finalizar_jogo.forEach((item)=>{
+          
+          item.addEventListener('click', handleChoice);
+        });
+      };
+      
+    function handleChoice(e){
+          if(e.target.id === 'yes') {
+            
+            document.querySelector('.timer-count').style.display = 'block';
+            
+            this.removeEventListener('click', handleChoice);
+            return recarregarPagina();
+          };
+      };
+      
+    function recarregarPagina(){
+        clock--;
+        document.querySelector('#timer').innerText = clock;
+        
+        if(clock === 0)
+          return location.reload();
+          
+        return timer = setTimeout(recarregarPagina,1000);
+      };
+    };
+  
+  function bot(){
+    
+    turno = jogador2;
+    let numeroAleatorio = gerarNumeroAleatorio();
+    
+     tabuleiro.forEach((item)=>{
+        let validacao = item.id == numeroAleatorio;
+        let containX = item.classList.contains('X');
+        let containO= item.classList.contains('O');
+  
+        if(!!validacao){
+          
+            if(validacao && !!containX || !!containO){
+              console.log(item);
+              return bot();
+            }
+            
+            else{
+              setTimeout(()=>{
+                item.classList.add(jogador2);
+                item.innerText = jogador2;
+              },300);
+              
+              return turno = jogador;
+            };
+        };
+     });
+    };
+    
+  function gerarNumeroAleatorio(){
+      
+      return Math.round(Math.random() * 9);
+    };
+    
 });
